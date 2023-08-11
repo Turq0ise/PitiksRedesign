3d2de329b8ee350a590afa7f2661c3fd72312edf
Turq0ise <72288865+Turq0ise@users.noreply.github.com>
2023-08-11 15:53:52 +0800

    Optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://turq0ise.github.io",
-    base: "/PitiksRedesign"
+    base: "/PitiksRedesign",
+    experimental: {
+        assets: true
+    }
 });
